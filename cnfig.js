24c495a81c92b478b540ca6b68fb659c5ef5eb63
OdoiOkoth <odoiokoth@gmail.com>
2022-08-28 08:23:49 +0300

    Version 1.1

---
+++ cnfig.js
@@ -0,0 +1,3 @@
+console.log('index.js');
+console.log('life of a programmer');
+console.log('version 1.1');
---
+++ index.js
@@ -0,0 +1,4 @@
+console.log('i love programming');
+console.log('software enginnnering is my life');
+console.log('config.js');
+console.log('version 1.1');
